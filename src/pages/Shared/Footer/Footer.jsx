a48da799b328c9e5491d2a49733a131aa66b13e0
mehedi9868 <mehedi9868@gmail.com>
2023-05-18 22:41:40 +0600

    footer updated with info and make responsive

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,33 +1,52 @@
 import { Link } from "react-router-dom";
 import logo from "../../../assets/logo.png";
+import { FaTwitter, FaInstagram, FaFacebook } from 'react-icons/fa';
 
 const Footer = () => {
     return (
 
-        <footer className="bg-[#262626] shadow dark:bg-gray-900 p-4">
+        <footer className="bg-[#262626] shadow p-4">
             <div className="w-full mx-auto p-4 md:py-8">
-                <div className="sm:flex sm:items-center sm:justify-between">
-                    <Link to="/" className="flex items-center mb-4 sm:mb-0">
-                        <img src={logo} className="h-10 mr-3" alt="Logo" />
-                        <h2 className='text-blue-200 text-3xl font-bold'>Mini Wheeles</h2>
-                    </Link>
-                    <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-200 sm:mb-0 dark:text-gray-400">
-                        <li>
-                            <Link to="/" className="mr-4 hover:underline md:mr-6 ">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/" className="mr-4 hover:underline md:mr-6">Privacy Policy</Link>
-                        </li>
-                        <li>
-                            <Link to="/" className="mr-4 hover:underline md:mr-6 ">Licensing</Link>
-                        </li>
-                        <li>
-                            <Link to="/" className="hover:underline">Contact</Link>
-                        </li>
-                    </ul>
+                <div className="grid md:grid-cols-4 items-center justify-center">
+                    <div>
+                        <Link to="/" className="flex items-center mb-4 sm:mb-0">
+                            <img src={logo} className="h-10 mr-3" alt="Logo" />
+                            <h2 className='text-gray-400 text-3xl font-bold'>Mini Wheeles</h2>
+                        </Link>
+                    </div>
+                    <div className="mb-3">
+                        <p className="text-gray-400">Address:</p>
+                        <p className="text-gray-400">
+                            Haemayetpur Pabna, Bangladesh,
+                        </p>
+                        <p className="text-gray-400">
+                            Postal Code: 6602
+                        </p>
+                    </div>
+
+                    <div className="text-gray-400 mb-3">
+                        <p className="mt-2">Contact Information:</p>
+                        <p className="text-gray-400">Phone: +880-1797-XXXXXX</p>
+                        <p className="text-gray-400">Email: info@gmail.com</p>
+                    </div>
+                    <div >
+                        <p className="text-gray-400 mb-1">Follow Us:</p>
+                        <div className="flex space-x-4">
+                            <a href="#" className="text-gray-400 hover:text-gray-300">
+                                <FaFacebook size={24} />
+                            </a>
+                            <a href="#" className="text-gray-400 hover:text-gray-300">
+                                <FaTwitter size={24} />
+                            </a>
+                            <a href="#" className="text-gray-400 hover:text-gray-300">
+                                <FaInstagram size={24} />
+                            </a>
+                        </div>
+                    </div>
                 </div>
+                {/* copyright */}
                 <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                <span className="block text-sm text-gray-400 sm:text-center dark:text-gray-400">© 2023 <Link to="/" className="hover:underline">Mini Wheels</Link>. All Rights Reserved.</span>
+                <span className="block text-sm text-gray-400 text-center dark:text-gray-400">© 2023 <Link to="/" className="hover:underline">Mini Wheels</Link>. All Rights Reserved.</span>
             </div>
         </footer>
 
