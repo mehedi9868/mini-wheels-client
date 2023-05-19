ce8dce834bd0dde0fc2bb69ff0f8f73cf3bc6e97
mehedi9868 <mehedi9868@gmail.com>
2023-05-19 20:03:13 +0600

    notify user to login first with modal

--- src/pages/Home/Categories/SubCategoryCard.jsx
+++ src/pages/Home/Categories/SubCategoryCard.jsx
@@ -1,7 +1,20 @@
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const SubCategoryCard = ({ car }) => {
 
+    const handleViewDetails = () => {
+        Swal.fire({
+            title: '<p>Please Login!</p>',
+            text: 'You have to log in first to view details',
+            confirmButtonText: 'Ok',
+            customClass: {
+                confirmButton: 'bg-blue-400 px-6 py-3 rounded-md text-white hover:bg-blue-500',
+            },
+            buttonsStyling: false,
+            icon: 'warning',
+        });
+    }
     return (
         <>
             <div className="flex gap-4 items-center px-6 py-2">
@@ -11,7 +24,7 @@ const SubCategoryCard = ({ car }) => {
                     <p className="font-bold">Price:</p>
                     <p className="font-bold">Rating:</p>
                     <Link to={`/details/${car.id}`}>
-                        <button className="bg-blue-400 text-white px-6 py-3 mt-2 rounded-md hover:bg-blue-500">View Details</button>
+                        <button onClick={handleViewDetails} className="bg-blue-400 text-white px-6 py-3 mt-2 rounded-md hover:bg-blue-500">View Details</button>
                     </Link>
                 </div>
             </div>
