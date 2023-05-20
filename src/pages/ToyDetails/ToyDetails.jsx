1f6346b23da7fdbad92cb640d722e8356d637e93
mehedi9868 <mehedi9868@gmail.com>
2023-05-20 08:54:28 +0600

    demo dynamic modal setup need update

--- src/pages/ToyDetails/ToyDetails.jsx
+++ src/pages/ToyDetails/ToyDetails.jsx
@@ -1,12 +1,55 @@
 import { useLoaderData } from "react-router-dom";
+import { FaStar } from "react-icons/fa";
 
 const ToyDetails = () => {
+    const singleCar = useLoaderData();
+    const { name, picture, seller, email, category, price, rating, quantity, description } = singleCar;
 
-    const singleCar = useLoaderData()
+    // rating
+    const renderRatingStars = () => {
+        const stars = [];
+        for (let i = 0; i < 5; i++) {
+            stars.push(
+                <FaStar
+                    key={i}
+                    className={i < rating ? "text-yellow-400" : "text-gray-300"}
+                />
+            );
+        }
+        return stars;
+    };
+
     return (
         <div>
-            <h2 className="text-2xl font-bold text-red-500 underline underline-offset-8 text-center my-20" style={{ fontFamily: 'Kalam, cursive' }}>Single Car Details</h2>
-            <p>Name: {singleCar.name}</p>
+            <label htmlFor="my-modal-6" className="btn">open modal</label>
+
+            <input type="checkbox" id="my-modal-6" className="modal-toggle" />
+            <div className="modal sm:modal-middle">
+                <div className="modal-box p-0 rounded-md">
+                    <img className="w-full h-auto" src={picture} alt="" />
+                    <div className="p-4 italic">
+                        <h2 className="text-xl font-bold text-red-500 my-2" style={{ fontFamily: 'Kalam, cursive' }}>{name}</h2>
+
+
+                        <div className="flex justify-between items-center">
+                            <div>
+                                <p className="text-gray-600 mb-2">Category: <span className="font-semibold ">{category}</span></p>
+                                <p className="text-gray-600 mb-2">Seller: <span className="font-semibold ">{seller}</span></p>
+                                <p className="text-gray-600 mb-2">Email: <span className="font-semibold ">{email}</span></p>
+                            </div>
+                            <div>
+                                <p className="text-gray-600 mb-2">Available Quantity: <span className="font-semibold ">{quantity}</span></p>
+                                <p className="flex items-center gap-1 mb-2">Rating: {renderRatingStars()}</p>
+                                <p className="text-gray-600 mb-2">Price: <span className="font-semibold ">{price}</span></p>
+                            </div>
+                        </div>
+                        <p className="text-gray-800 text-justify">Description: {description}</p>
+                    </div>
+                    <div className="modal-action">
+                        <label htmlFor="my-modal-6" className="btn w-full border-none rounded-none bg-blue-500 hover:bg-blue-600">close</label>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
